31296aebe037b10675843452c68c064c0eeec10f
tproykov <tproykov@gmail.com>
2024-10-20 18:50:32 +0100

    update cup

--- SoftUniada2024/SU02CupProblem.java
+++ SoftUniada2024/SU02CupProblem.java
@@ -10,17 +10,17 @@ public class SU02CupProblem {
 
         int n = Integer.parseInt(scanner.nextLine());
 
-        for (int row = 1; row <= (n * 2) + 4; row++) {
+        for (int row = 0; row < (n / 2); row++) {
 
-            for (int i = 1; i <= n; i++) {
+            for (int i = 1; i <= n + row; i++) {
                 System.out.print(".");
             }
-            for (int j = n + 1; j <= n * 5 - n; j++) {
+            for (int j = n * 3; j >= 0; j--) {
                 System.out.print("#");
             }
-            for (int k = 1; k <= n; k++) {
-                System.out.print(".");
-            }
+//            for (int k = 1; k <= n; k++) {
+//                System.out.print(".");
+//            }
             System.out.println();
 
         }
