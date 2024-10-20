072de7c53747f3f9f716e6cec65bbd28c70f13b8
tproykov <tproykov@gmail.com>
2024-10-20 20:20:33 +0100

    third task

--- SoftUniada2024/SU03LargestNumbers.java
+++ SoftUniada2024/SU03LargestNumbers.java
@@ -1,6 +1,7 @@
 package SoftUniada2024;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SU03LargestNumbers {
 
@@ -8,8 +9,23 @@ public class SU03LargestNumbers {
 
         Scanner scanner = new Scanner(System.in);
 
-        
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        int n = Integer.parseInt(scanner.nextLine());
 
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            list.add(numbers[i]);
+        }
+
+        List<Integer> sortedNumbers = list.stream().sorted().collect(Collectors.toList());
+
+        Collections.reverse(sortedNumbers);
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            System.out.println(sortedNumbers.get(i));
+        }
     }
-
 }
