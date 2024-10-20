9d8b2811ad3251fb5ae6f12e8310cfbfa33e7c72
tproykov <tproykov@gmail.com>
2024-10-20 20:03:27 +0100

    update cup

--- SoftUniada2024/SU02CupProblem.java
+++ SoftUniada2024/SU02CupProblem.java
@@ -15,7 +15,7 @@ public class SU02CupProblem {
             for (int a = 1; a <= n + row; a++) {
                 System.out.print(".");
             }
-            for (int b = n * 3 - row * 2; b >= 0; b--) {
+            for (int b = n * 3 - row * 2; b > 0; b--) {
                 System.out.print("#");
             }
             for (int c = 1; c <= n + row; c++) {
@@ -30,7 +30,7 @@ public class SU02CupProblem {
                 System.out.print(".");
             }
             System.out.print("#");
-            for (int e = 2 * n - 2 - row * 2; e >= 0; e--) {
+            for (int e = 2 * n - 2 - row * 2; e > 0; e--) {
                 System.out.print(".");
             }
             System.out.print("#");
@@ -40,6 +40,53 @@ public class SU02CupProblem {
             System.out.println();
         }
 
+        for (int g = 1; g <= n * 2; g++) {
+            System.out.print(".");
+        }
+        for (int h = 1; h <= n; h++) {
+            System.out.print("#");
+        }
+        for (int i = 1; i <= n * 2; i++) {
+            System.out.print(".");
+        }
+        System.out.println();
 
+        for (int row = 1; row <= n / 2; row++) {
+
+            for (int j = 1; j <= n * 2 - 2; j++) {
+                System.out.print(".");
+            }
+            for (int k = 1; k <= n + 4; k++) {
+                System.out.print("#");
+            }
+            for (int l = 1; l <= n * 2 - 2; l++) {
+                System.out.print(".");
+            }
+            System.out.println();
+        }
+
+        for (int m = 1; m <= 5 * n / 2 - 5; m++) {
+            System.out.print(".");
+        }
+        System.out.print("D^A^N^C^E^");
+
+        for (int o = 1; o <= 5 * n / 2 - 5; o++) {
+            System.out.print(".");
+        }
+        System.out.println();
+
+        for (int row = 1; row <= n / 2 + 1; row++) {
+
+            for (int p = 1; p <= n * 2 - 2; p++) {
+                System.out.print(".");
+            }
+            for (int q = 1; q <= n + 4; q++) {
+                System.out.print("#");
+            }
+            for (int r = 1; r <= n * 2 - 2; r++) {
+                System.out.print(".");
+            }
+            System.out.println();
+        }
     }
 }
