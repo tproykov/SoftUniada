9a6cae0a397b27d2c5d11be89c33adaff90419b5
tproykov <tproykov@gmail.com>
2024-10-21 11:01:43 +0100

    Create SU04Chess.java

---
+++ SoftUniada2024/SU04Chess.java
@@ -0,0 +1,18 @@
+package SoftUniada2024;
+
+import java.util.Scanner;
+
+public class SU04Chess {
+
+    Scanner scanner = new Scanner(System.in);
+
+    int chessBoardSize = Integer.parseInt(scanner.nextLine());
+    int startRow = Integer.parseInt(scanner.nextLine());
+    int startColumn = Integer.parseInt(scanner.nextLine());
+    int endRow = Integer.parseInt(scanner.nextLine());
+    int endColumn = Integer.parseInt(scanner.nextLine());
+
+
+
+
+}
