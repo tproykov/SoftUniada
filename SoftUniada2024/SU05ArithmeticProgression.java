54025e2449fc3d03c4d59868d28ddd7f30f22162
tproykov <tproykov@gmail.com>
2024-10-27 00:14:18 +0100

    update

--- SoftUniada2024/SU05ArithmeticProgression.java
+++ SoftUniada2024/SU05ArithmeticProgression.java
@@ -9,11 +9,14 @@ public class SU05ArithmeticProgression {
 
         Scanner scanner = new Scanner(System.in);
 
+        // read the input from the console
         int[] integerArray = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt)
                 .toArray();
 
+        // length of the array
         int n = integerArray.length;
 
+        // apply the method of finding the count of arithmetic progressions
         int count = countArithmeticProgressions(integerArray, n);
         System.out.println(count);
     }
@@ -24,7 +27,7 @@ public class SU05ArithmeticProgression {
         // iterate through all possible sub-arrays (combination of elements)
         int totalCountProgressions = 0;
         for (int i = 0; i < integerArray.length; i++) {
-            for (int j = i + 1; j <= integerArray.length; j++) {
+            for (int j = i + 1; j < integerArray.length; j++) {
                 if (isArithmeticProgression(integerArray, i, j)) {
                     totalCountProgressions++;
                 }
@@ -34,11 +37,11 @@ public class SU05ArithmeticProgression {
     }
 
     // check if a subsequence is an arithmetic progression
-    public static boolean isArithmeticProgression(int[] array, int start, int end) {
-        if (end - start <= 2) return true;  // Single element or empty is trivially AP
+    public static boolean isArithmeticProgression(int[] array, int startIndex, int endIndex) {
+        if (endIndex - startIndex <= 2) return true;  // Single element or empty is trivially AP
 
-        int diff = array[start + 1] - array[start];
-        for (int i = start + 1; i < end - 1; i++) {
+        int diff = array[startIndex + 1] - array[startIndex];
+        for (int i = startIndex + 1; i < endIndex - 1; i++) {
             if (array[i + 1] - array[i] != diff) {
                 return false;  // If the difference between consecutive elements is not constant
             }
